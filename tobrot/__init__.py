0a9ed03f914ea8978250f7f7dc6ef284717bde30
sfah007 <69042482+sfah007@users.noreply.github.com>
2021-03-03 21:52:53 +0300

    Update __init__.py

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -81,12 +81,3 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
-def multi_rclone_init():
-    if not os.path.exists('rclone_bak.conf'):  # backup rclone.conf file
-        with open('rclone_bak.conf', 'w+', newline="\n", encoding='utf-8') as fole:
-            with open('rclone.conf', 'r') as f:
-                fole.write(f.read())
-        LOGGER.info("rclone.conf backuped to rclone_bak.conf!")
-
-
-multi_rclone_init()
